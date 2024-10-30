2e6a735c2cd9465ff9b4e38b0ba9c5bc71f1e3fe
MicheleFumi <michele.fumi96@gmail.com>
2024-10-30 17:29:38 +0100

    fix img bug

--- Data/post.js
+++ Data/post.js
@@ -4,31 +4,31 @@ const post = [
     {
         title: 'My First trip',
         content:'Visiting London for the first time!',
-        img:'./public/imgs/img1.png',
+        img:'./imgs/img1.png',
         tags:['London, Traveller, Experience, HarryPotter']
 },
 {
     title: 'A Must See Place: Camden Town!',
     content:'Getting lost through the neighbor!',
-    img:'./public/imgs/img2.png',
+    img:'./imgs/img2.png',
     tags:['London, Traveller, CamdenLock, Cyberdog, Punk']
 },
 {
     title: 'Quick Stop At Embankment',
     content:'Enjoying the city view!',
-    img:'./public/imgs/img3.png',
+    img:'./imgs/img3.png',
     tags:['London, Traveller, RiverThames, Sunset, GoldenHour, CitySkyline ']
 },
 {
     title: 'The Royal Show',
     content:'Seeing the change of the guards live!',
-    img:'./public/imgs/img4.png',
+    img:'./imgs/img4.png',
     tags:['London, Traveller,Royal Palace, ChangeOfGuardsLondon, VisitLondon']
 },
 {
     title: 'Experience A True Pub Delicatessen',
     content:'at certain hour you must grab a pint!',
-    img:'./public/imgs/img5.png',
+    img:'./imgs/img5.png',
     tags:['London, Traveller, Beer, Pub']
 },
 ]
--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express')
-
 const app = express()
 app.use(express.static('public'))
 const host = 'http://127.0.0.1'
