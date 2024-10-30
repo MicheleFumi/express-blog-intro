f262714f256646d12d82638d41096323021cbd45
MicheleFumi <michele.fumi96@gmail.com>
2024-10-30 15:21:42 +0100

    bug fix

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.static('public'))
 const host = 'http://127.0.0.1'
 const port = 3000
 const postController = require('./Controllers/postsController')
@@ -9,8 +10,10 @@ app.listen(port, () => {
 })
 
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
     res.send('<H1>Benvenuti nel mio blog!</H1>')
   })
 
   app.get('/post' ,postController.index)
+
+  
