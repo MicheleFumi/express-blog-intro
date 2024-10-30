c608191fc50ff6a4787c5cac2296bc695cb3abef
MicheleFumi <michele.fumi96@gmail.com>
2024-10-30 14:49:31 +0100

    add post object

--- main.js
+++ main.js
@@ -12,3 +12,36 @@ app.listen(port, () => {
 app.get('/', (req, res) => {
     res.send('<H1>Benvenuti nel mio blog!</H1>')
   })
+
+  const post = [
+    {
+        title: 'My First trip',
+        content:'Visiting London for the first time!',
+        img:'./public/img1.webbp',
+        tags:['London, Traveller, Experience, HarryPotter']
+},
+{
+    title: 'A Must See Place: Camden Town!',
+    content:'Getting lost through the neighbor!',
+    img:'./public/img2.webb',
+    tags:['London, Traveller, CamdenLock, Cyberdog, Punk']
+},
+{
+    title: 'Quick Stop At Embankment',
+    content:'Enjoying the city view!',
+    img:'./public/img3.webb',
+    tags:['London, Traveller, RiverThames, Sunset, GoldenHour, CitySkyline ']
+},
+{
+    title: 'The Royal Show',
+    content:'Seeing the change of the guards live!',
+    img:'./public/img4.webb',
+    tags:['London, Traveller,Royal Palace, ChangeOfGuardsLondon, VisitLondon']
+},
+{
+    title: 'Experience A True Pub Delicatessen',
+    content:'at certain hour you must grab a pint!',
+    img:'./public/img5.webb',
+    tags:['London, Traveller, Beer, Pub']
+},
+]
